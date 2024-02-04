454654f88b4c249602369a866b629d3236917787
jose antonio alatorre chavez <ja54312@MacBook-Pro-de-jose.local>
2024-02-04 10:40:30 -0600

    ajustes eventos

--- src/components/EVENTOS/eventos.js
+++ src/components/EVENTOS/eventos.js
@@ -4,47 +4,46 @@ const ValoresEventos = [
     pharagraph:
       "En las bodas, nos aseguramos de que cada detalle esté cuidado, desde la recepción con elegantes cócteles hasta la barra libre en la fiesta, brindando a tus invitados...",
     subtitle: "una experiencia inolvidable",
-    imgUrl: "./BodasImage.jpeg",
+    imgUrl: "https://eternology.com.mx/wp-content/uploads/2024/02/BodasImage-scaled.jpeg",
   },
   {
     title: "XV Años",
     pharagraph:
       "Para los XV años, creamos un ambiente mágico con cócteles y bebidas personalizadas que complementan perfectamente la temática...",
     subtitle: "y la energía de la celebración",
-    imgUrl: "./xvaños.jpeg",
+    imgUrl: "https://eternology.com.mx/wp-content/uploads/2024/02/xvanos.jpeg",
   },
   {
     title: "Bautizos",
     pharagraph:
       "En los bautizos, nos encargamos de crear una atmósfera cálida y acogedora, con una selección de bebidas refrescantes para que tus seres queridos...",
     subtitle: "disfruten de este día especial",
-    imgUrl: "./bautizos.png",
+    imgUrl: "https://eternology.com.mx/wp-content/uploads/2024/02/bautizos.png",
   },
   {
     title: "Graduaciones",
     pharagraph:
       "En las graduaciones, celebramos los logros y el inicio de una nueva etapa. Nuestras barras libres ofrecen una amplia selección de bebidas para que los graduados y sus amigos...",
     subtitle: "brinden por el éxito futuro",
-    imgUrl: "./graduaciones.png",
+    imgUrl: "https://eternology.com.mx/wp-content/uploads/2024/02/graduaciones.png",
   },
   {
     title: "Fiestas",
     pharagraph:
       "En las fiestas de cumpleaños, nos sumergimos en la diversión y la alegría. Preparamos cócteles creativos y emocionantes para garantizar que cada invitado tenga...",
     subtitle: "un momento inolvidable",
-    imgUrl: "./fiestas.jpeg",
+    imgUrl: "https://eternology.com.mx/wp-content/uploads/2024/02/fiestas.jpeg",
   },
   {
     title: "Eventos Ejecutivos",
     pharagraph:
       "En eventos empresariales, nos aseguramos de que cada detalle refleje profesionalismo y excelencia. Nuestro equipo de mixólogos expertos creará bebidas sofisticadas que...",
     subtitle: "impresionarán a tus clientes y colegas",
-    imgUrl: "./eventosEjecutivos.png",
+    imgUrl: "https://eternology.com.mx/wp-content/uploads/2024/02/eventosEjecutivos.png",
   },
 ];
 
 function handleChange(value) {
-  console.log("change", value);
   let NumberEvent = 0;
   if (value == "opcion2") {
     NumberEvent = 1;
