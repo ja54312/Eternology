e7ba239729dfe97948af26de027f79726ecb7c01
JA54312 <ija54312@gmail.com>
2024-06-09 12:58:45 -0600

    sliderRecetasCategoria

---
+++ src/components/BLOG/index.js
@@ -0,0 +1,34 @@
+// Importacion de valores globales de Slider
+var Slider = window.SliderRecetasCategoria;
+
+function MiComponente(Slider) {
+  let NumCards = Slider.length;
+  console.log("NumSlider", NumCards);
+  console.log("CArDs", Slider);
+
+  if (NumCards === 0) {
+    return `<div class="NoCategory">No hay Categorias disponibles</div>`;
+  } else if (NumCards > 0) {
+    return (
+      Slider &&
+      Slider.map(
+        (element) =>
+          `<div class="cardContainer">
+                  <div class="cardImageContainer">
+                    <img src="${element?.urlImgage}" alt="receta-${element?.index}" />
+                  </div>
+                  <div class="cardTextContainer">
+                    <span>${element?.titleCategory}</span>
+                  </div>
+              </div>`
+      )
+    );
+  }
+}
+
+// Función para renderizar un componente en el contenedor
+function renderizar(componente, contenedorId) {
+  document.getElementById(contenedorId).innerHTML = componente;
+}
+// Renderizar el componente inicial
+renderizar(MiComponente(SliderRecetasCategoria), "miSliderRecetasCategoria");
---
+++ src/components/BLOG/values.js
@@ -0,0 +1,40 @@
+var SliderRecetasCategoria = [
+  {
+    index: 5,
+    urlImgage:
+      "https://eternology.com.mx/wp-content/uploads/2024/02/graduaciones.png",
+    titleCategory: "Cocteles Con Ron",
+  },
+  {
+    index: 4,
+    urlImgage:
+      "https://eternology.com.mx/wp-content/uploads/2024/02/eventos2.jpeg",
+    titleCategory: "Cocteles con Ron",
+  },
+  {
+    index: 3,
+    urlImgage:
+      "https://eternology.com.mx/wp-content/uploads/2024/02/fiestas.jpeg",
+    titleCategory: "Cocteles con Ron",
+  },
+  {
+    index: 2,
+    urlImgage:
+      "https://eternology.com.mx/wp-content/uploads/2024/02/bebidas1.jpeg",
+    titleCategory: "Cocteles con Ron",
+  },
+  {
+    index: 1,
+    urlImgage:
+      "https://eternology.com.mx/wp-content/uploads/2024/02/bebidas2.jpeg",
+    titleCategory: "Cocteles con Ron",
+  },
+  {
+    index: 0,
+    urlImgage:
+      "https://eternology.com.mx/wp-content/uploads/2024/02/BodasImage-scaled.jpeg",
+    titleCategory: "Cocteles con Ron",
+  },
+];
+
+window.SliderRecetasCategoria = SliderRecetasCategoria;
